5912c4c1098358c3d8c669b845ec7b042ebdcd5c
Hiren <rs.hirenrathod@gmail.com>
2021-01-09 17:31:29 +0530

    Initial commit

---
+++ app/src/main/java/com/royalsoftsolutions/image_drowing_project/ContraintLayout3Activity.java
@@ -0,0 +1,14 @@
+package com.royalsoftsolutions.image_drowing_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ContraintLayout3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contraint_layout3);
+    }
+}
---
+++ app/src/main/java/com/royalsoftsolutions/image_drowing_project/visibiltyActivity.java
@@ -0,0 +1,14 @@
+package com.royalsoftsolutions.image_drowing_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class visibiltyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visibilty);
+    }
+}
